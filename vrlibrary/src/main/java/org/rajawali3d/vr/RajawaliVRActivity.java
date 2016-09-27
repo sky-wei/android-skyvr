821dbd75b40fb7100a00485d3cf6fe3189f67cd3
zhouwei <zhouwei@weitoo.com>
2016-09-27 15:16:15 +0800

    添加OpenGL框架

---
+++ vrlibrary/src/main/java/org/rajawali3d/vr/RajawaliVRActivity.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2015 Dennis Ippel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.rajawali3d.vr;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.vr.sdk.base.GvrActivity;
+import com.google.vr.sdk.base.GvrView;
+
+import org.rajawali3d.vr.renderer.RajawaliVRRenderer;
+
+/**
+ * @author dennis.ippel
+ */
+public class RajawaliVRActivity extends GvrActivity {
+    private GvrView mSurfaceView;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+        mSurfaceView = new GvrView(this);
+
+        addContentView(mSurfaceView, new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT));
+
+        mSurfaceView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+	            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+	            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+        setGvrView(mSurfaceView);
+	}
+	
+	protected void setRenderer(RajawaliVRRenderer renderer) {
+	    mSurfaceView.setRenderer(renderer);
+	}
+
+    public GvrView getSurfaceView() {
+        return mSurfaceView;
+    }
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+}
+
---
+++ vrlibrary/src/main/java/org/rajawali3d/vr/surface/RajawaliVRSurfaceView.java
@@ -0,0 +1,89 @@
+package org.rajawali3d.vr.surface;
+
+/**
+ * Copyright 2015 Dennis Ippel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.google.vr.sdk.base.GvrView;
+
+import org.rajawali3d.surface.IRajawaliSurface;
+import org.rajawali3d.surface.IRajawaliSurfaceRenderer;
+
+/**
+ *
+ */
+public class RajawaliVRSurfaceView extends GvrView implements IRajawaliSurface {
+
+    public RajawaliVRSurfaceView(Context context) {
+        super(context);
+    }
+
+    public RajawaliVRSurfaceView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onVisibilityChanged(View changedView, int visibility) {
+        if (visibility == View.GONE || visibility == View.INVISIBLE) {
+            onPause();
+        } else {
+            onResume();
+        }
+        super.onVisibilityChanged(changedView, visibility);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        onResume();
+    }
+
+    @Override
+    public void setFrameRate(double v) {
+
+    }
+
+    @Override
+    public int getRenderMode() {
+        return 0;
+    }
+
+    @Override
+    public void setRenderMode(int mode) {
+
+    }
+
+    @Override
+    public void setAntiAliasingMode(ANTI_ALIASING_CONFIG anti_aliasing_config) {
+
+    }
+
+    @Override
+    public void setSampleCount(int i) {
+
+    }
+
+    @Override
+    public void setSurfaceRenderer(IRajawaliSurfaceRenderer iRajawaliSurfaceRenderer) throws IllegalStateException {
+
+    }
+
+    @Override
+    public void requestRenderUpdate() {
+//        requestRender();
+    }
+}
