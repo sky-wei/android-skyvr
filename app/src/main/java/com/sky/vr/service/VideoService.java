c3cf4d1e724493be456f5dd946ce386a2fcc5617
zhouwei <zhouwei@weitoo.com>
2016-10-10 13:59:36 +0800

    修改toolbar上移的问题

--- app/src/main/java/com/sky/vr/service/VideoService.java
+++ app/src/main/java/com/sky/vr/service/VideoService.java
@@ -3,6 +3,7 @@ package com.sky.vr.service;
 import com.sky.vr.data.mojing.TagsResource;
 import com.sky.vr.data.mojing.Tags;
 
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import rx.Observable;
@@ -13,12 +14,14 @@ import rx.Observable;
 
 public interface VideoService {
 
-    @POST("1/tags_list.js")
+    @GET("1/tags_list.js")
     Observable<Tags> getCategory();
 
-    @POST("1/category/catinfo/{resId}-start{start}-num{num}.js")
-    Observable<TagsResource> getCategoryCatInfo(@Path("resId") int resId, @Path("start") int start, @Path("num") int num);
+    @GET("1/category/catinfo/{resId}-start{start}-num{num}.js")
+    Observable<TagsResource> getCategoryCatInfo(
+            @Path("resId") int resId, @Path("start") int start, @Path("num") int num);
 
-    @POST("1/tags_resource-type{resId}-tag{tag}-start{start}-num{num}.js")
-    Observable<TagsResource> getTagsResource(@Path("resId") int resId, @Path("tag") int tag, @Path("start") int start, @Path("num") int num);
+    @GET("1/tags_resource-type{resId}-tag{tag}-start{start}-num{num}.js")
+    Observable<TagsResource> getTagsResource(
+            @Path("resId") int resId, @Path("tag") int tag, @Path("start") int start, @Path("num") int num);
 }
